4397c0552e0d1b5b1e1f3bbc94180010cfd07f60
Kanasansoft <kanasansoft@gmail.com>
2010-03-27 17:45:16 +0900

    web.xmlは使用していないが、getNamedDispatcherでdefaultを取得しforward先を指定した。WebSocketServletを継承したWSServletのdoGetをOverrideした。
    @Override
    protected void doGet(HttpServletRequest request, HttpServletResponse response)
    		throws ServletException, IOException {
    	getServletContext().getNamedDispatcher("default").forward(request, response);
    }

--- src/main/java/com/kanasansoft/WebSocketRemote/WSServlet.java
+++ src/main/java/com/kanasansoft/WebSocketRemote/WSServlet.java
@@ -8,9 +8,15 @@ import org.eclipse.jetty.websocket.WebSocketServlet;
 class WSServlet extends WebSocketServlet {
 	private static final long serialVersionUID = 1L;
 	OnMessageObserver onMessageObserver = null;
+
 	public WSServlet(OnMessageObserver onMessageObserver) {
 		super();
 		this.onMessageObserver = onMessageObserver;
+	}
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		getServletContext().getNamedDispatcher("default").forward(request, response);
 	}
 	@Override
 	protected WebSocket doWebSocketConnect(HttpServletRequest request, String protocol) {
