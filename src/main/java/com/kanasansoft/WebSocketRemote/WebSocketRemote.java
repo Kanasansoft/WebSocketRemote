bb8046a95362f7adc5870085169e05843222ba4f
Kanasansoft <kanasansoft@gmail.com>
2010-04-03 13:12:09 +0900

    画像送信のリクエストがあった時だけ、リクエストをしたクライアントのみに画像を転送するようにした。

--- src/main/java/com/kanasansoft/WebSocketRemote/WebSocketRemote.java
+++ src/main/java/com/kanasansoft/WebSocketRemote/WebSocketRemote.java
@@ -7,6 +7,7 @@ import java.awt.Toolkit;
 import java.awt.TrayIcon;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.Date;
@@ -61,7 +62,7 @@ public class WebSocketRemote implements OnMessageObserver, OnCaptureObserver{
 		wsServletContextHandler.setContextPath("/");
 		server.setHandler(wsServletContextHandler);
 		ServletHolder wsServletHolder = new ServletHolder(wsServlet);
-//		wsServletHolder.setInitParameter("bufferSize", Integer.toString(8192*128,10));
+		wsServletHolder.setInitParameter("bufferSize", Integer.toString(8192*256,10));
 		wsServletContextHandler.addServlet(wsServletHolder, "/ws/*");
 		HandlerList handlerList = new HandlerList();
 		handlerList.setHandlers(new Handler[] {resourceHandler, wsServletContextHandler});
@@ -71,55 +72,6 @@ public class WebSocketRemote implements OnMessageObserver, OnCaptureObserver{
 		Capture capture = new Capture();
 		capture.setOnCaptureObserver(this);
 		capture.start();
-
-//		byte startByte = "^".getBytes()[0];
-//		byte endByte = "$".getBytes()[0];
-		byte separatorByte = "_".getBytes()[0];
-
-		while(true){
-//			screenData = capture.getScreenData();
-			if(screenData!=null){
-				byte[] base64 = screenData.base64;
-				if(base64!=null){
-//					System.out.println(base64.length);
-//					WebSocketDesktop.sendMessageAll((byte)WebSocket.SENTINEL_FRAME, base64,0,base64.length);
-/*
-					byte[] dataLength = Integer.toString(base64.length, 16).getBytes();
-					byte[] sendArray = new byte[base64.length+dataLength.length+3];
-				    System.arraycopy(dataLength, 0, sendArray, 1,dataLength.length);
-				    System.arraycopy(base64, 0, sendArray, dataLength.length+2,base64.length);
-				    sendArray[0] = startByte;
-				    sendArray[sendArray.length-1] = endByte;
-				    sendArray[dataLength.length+1] = separatorByte;
-					WebSocketDesktop.sendMessageAll((byte)WebSocket.SENTINEL_FRAME, sendArray,0,sendArray.length);
-*/
-					int sendSize = 4096;
-					int sendCount = base64.length / sendSize;
-					int remainder = base64.length % sendSize;
-					if(remainder!=0){
-						sendCount++;
-					}
-					byte[] imageId = Long.toString(new Date().getTime(),16).getBytes();
-					byte[] sequenceCount = Integer.toString(sendCount, 16).getBytes();
-					for(int i=0;i<sendCount;i++){
-						byte[] sequenceNumber = Integer.toString(i + 1, 16).getBytes();
-						int restLength = base64.length-i * sendSize;
-						int sendLength = sendSize<restLength?sendSize:restLength;
-						byte[] sendData = new byte[imageId.length + sequenceNumber.length + sequenceCount.length + sendLength + 3];
-						System.arraycopy(imageId, 0, sendData, 0, imageId.length);
-						System.arraycopy(sequenceNumber, 0, sendData, imageId.length + 1, sequenceNumber.length);
-						System.arraycopy(sequenceCount, 0, sendData, imageId.length + sequenceNumber.length + 2, sequenceCount.length);
-						System.arraycopy(base64, i * sendSize, sendData, imageId.length + sequenceNumber.length + sequenceCount.length + 3, sendLength);
-						sendData[imageId.length + 0] = separatorByte;
-						sendData[imageId.length + sequenceNumber.length + 1] = separatorByte;
-						sendData[imageId.length + sequenceNumber.length + sequenceCount.length + 2] =separatorByte;
-						WebSocketDesktop.sendMessageAll((byte)WebSocket.SENTINEL_FRAME, sendData,0,sendData.length);
-					}
-
-				}
-			}
-			Thread.sleep(1000);
-		}
 
 	}
 
@@ -135,6 +87,36 @@ public class WebSocketRemote implements OnMessageObserver, OnCaptureObserver{
 
 	@Override
 	synchronized public void onMessage(Outbound outbound, byte frame, String data) {
+		byte separatorByte = "_".getBytes()[0];
+		if(screenData==null){return;}
+		byte[] base64 = screenData.base64;
+		if(base64==null){return;}
+		int sendSize = 8192;
+		int sendCount = base64.length / sendSize;
+		int remainder = base64.length % sendSize;
+		if(remainder!=0){
+			sendCount++;
+		}
+		byte[] imageId = Long.toString(new Date().getTime(),16).getBytes();
+		byte[] sequenceCount = Integer.toString(sendCount, 16).getBytes();
+		for(int i=0;i<sendCount;i++){
+			byte[] sequenceNumber = Integer.toString(i + 1, 16).getBytes();
+			int restLength = base64.length-i * sendSize;
+			int sendLength = sendSize<restLength?sendSize:restLength;
+			byte[] sendData = new byte[imageId.length + sequenceNumber.length + sequenceCount.length + sendLength + 3];
+			System.arraycopy(imageId, 0, sendData, 0, imageId.length);
+			System.arraycopy(sequenceNumber, 0, sendData, imageId.length + 1, sequenceNumber.length);
+			System.arraycopy(sequenceCount, 0, sendData, imageId.length + sequenceNumber.length + 2, sequenceCount.length);
+			System.arraycopy(base64, i * sendSize, sendData, imageId.length + sequenceNumber.length + sequenceCount.length + 3, sendLength);
+			sendData[imageId.length + 0] = separatorByte;
+			sendData[imageId.length + sequenceNumber.length + 1] = separatorByte;
+			sendData[imageId.length + sequenceNumber.length + sequenceCount.length + 2] =separatorByte;
+			try {
+				outbound.sendMessage((byte)WebSocket.SENTINEL_FRAME, sendData,0,sendData.length);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 	@Override
