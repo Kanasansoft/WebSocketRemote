913162ee60362971bc0c4189f6fdf061d7ef3855
Kanasansoft <kanasansoft@gmail.com>
2010-04-12 22:46:53 +0900

    ScreenData内で画面IDと時間を持つように変更。

--- src/main/java/com/kanasansoft/WebSocketRemote/ScreenData.java
+++ src/main/java/com/kanasansoft/WebSocketRemote/ScreenData.java
@@ -1,19 +1,29 @@
 package com.kanasansoft.WebSocketRemote;
 
 import java.awt.Rectangle;
+import java.util.Date;
 
 class ScreenData {
+	static long imageIdSequence=0;
+	long imageId;
+	Date date;
 	byte[] base64 = null;
 	Rectangle rect = null;
 	public ScreenData() {
+		this.imageId = ++imageIdSequence;
+		this.date = new Date();
 		this.base64 = null;
 		this.rect = null;
 	}
-	public ScreenData(byte[] base64, Rectangle rect) {
+	private ScreenData(long imageId, Date date, byte[] base64, Rectangle rect) {
+		this.imageId = imageId;
+		this.date = new Date(date.getTime());
 		this.base64 = base64.clone();
 		this.rect = new Rectangle(rect.x, rect.y, rect.width, rect.height);
 	}
 	synchronized void set(byte[] base64, Rectangle rect) {
+		this.imageId = ++imageIdSequence;
+		this.date = new Date();
 		this.base64 = base64;
 		this.rect = rect;
 	}
@@ -21,6 +31,6 @@ class ScreenData {
 		if(this.base64==null||this.rect==null){
 			return null;
 		}
-		return new ScreenData(this.base64, this.rect);
+		return new ScreenData(this.imageId, this.date, this.base64, this.rect);
 	}
 }
